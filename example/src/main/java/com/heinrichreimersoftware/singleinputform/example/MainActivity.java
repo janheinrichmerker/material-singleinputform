af3fa63e0dfc3347fb1cf73713a33fcbfffbe530
skimarxall <skimarxall@gmail.com>
2015-04-21 16:13:50 +0200

    #1 Fix global structure

--- example/src/main/java/com/heinrichreimersoftware/singleinputform/example/MainActivity.java
+++ example/src/main/java/com/heinrichreimersoftware/singleinputform/example/MainActivity.java
@@ -21,91 +21,126 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.heinrichreimersoftware.singleinputform.SingleInputFormActivity;
 import com.heinrichreimersoftware.singleinputform.steps.CheckBoxStep;
 import com.heinrichreimersoftware.singleinputform.steps.DateStep;
+import com.heinrichreimersoftware.singleinputform.steps.OptionStep;
 import com.heinrichreimersoftware.singleinputform.steps.SeekBarStep;
 import com.heinrichreimersoftware.singleinputform.steps.Step;
 import com.heinrichreimersoftware.singleinputform.steps.TextStep;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-public class MainActivity extends SingleInputFormActivity{
+public class MainActivity extends SingleInputFormActivity {
 
-	private static final String DATA_KEY_HEIGHT = "height";
-	private static final String DATA_KEY_EULA = "eula";
-    private static final String DATA_KEY_EMAIL = "email";
-	private static final String DATA_KEY_PASSWORD = "password";
-	private static final String DATA_KEY_BIRTHDAY = "birthday";
-	private static final String DATA_KEY_CITY = "city";
+  private static final String DATA_KEY_HEIGHT = "height";
+  private static final String DATA_KEY_EULA = "eula";
+  private static final String DATA_KEY_EMAIL = "email";
+  private static final String DATA_KEY_PASSWORD = "password";
+  private static final String DATA_KEY_BIRTHDAY = "birthday";
+  private static final String DATA_KEY_OPTIONS = "options";
 
-	@Override
-	protected List<Step> getSteps(Context context){
-		List<Step> steps = new ArrayList<Step>();
-        steps.add(
-                new CheckBoxStep(context, DATA_KEY_EULA, R.string.eula, R.string.eula_title, R.string.eula_error, R.string.eula_details, new CheckBoxStep.StepChecker() {
-                    @Override
-                    public boolean check(boolean input) {
-                        return input;
-                    }
-                })
-        );
-        steps.add(
-                new TextStep(context, DATA_KEY_EMAIL, InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS, R.string.email, R.string.email_error, R.string.email_details, new TextStep.StepChecker() {
-                    @Override
-                    public boolean check(String input) {
-                        return android.util.Patterns.EMAIL_ADDRESS.matcher(input).matches();
-                    }
-                })
-        );
-		steps.add(
-				new TextStep(context, DATA_KEY_PASSWORD, InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD, R.string.password, R.string.password_error, R.string.password_details, new TextStep.StepChecker() {
-					@Override
-					public boolean check(String input) {
-						return input.length() >= 5;
-					}
-				})
-		);
-		steps.add(
-				new DateStep(context, DATA_KEY_BIRTHDAY, R.string.birthday, R.string.birthday_error, R.string.birthday_details, new DateStep.StepChecker(){
-					@Override
-					public boolean check(int year, int month, int day){
-						Calendar today = new GregorianCalendar();
-						Calendar birthday = new GregorianCalendar(year, month, day);
-						today.add(Calendar.YEAR, -14);
-						return today.after(birthday);
-					}
-				})
-		);
-		steps.add(
-				new SeekBarStep(context, DATA_KEY_HEIGHT, 150, 180, R.string.height, R.string.height_error, R.string.height_details, new SeekBarStep.StepChecker() {
-					@Override
-					public boolean check(int progress) {
-						return progress >= 160;
-					}
-				})
-		);
-		steps.add(
-				new TextStep(context, DATA_KEY_CITY, InputType.TYPE_CLASS_TEXT, R.string.city, R.string.city_error, R.string.city_details)
-		);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
 
-		return steps;
-	}
+  @Override
+  protected List<Step> getSteps(Context context) {
+    long startTime = System.currentTimeMillis();
+    List<Step> steps = new ArrayList<Step>();
+    steps.add(new CheckBoxStep.CheckBoxStepBuilder().setContext(context)
+        .setDataKey(DATA_KEY_EULA)
+        .setTextResId(R.string.eula)
+        .setTitleResId(R.string.eula_title)
+        .setErrorResId(R.string.eula_error)
+        .setDetailsResId(R.string.eula_details)
+        .setChecker(new CheckBoxStep.StepChecker() {
+          @Override
+          public boolean check(boolean input) {
+            return input;
+          }
+        })
+        .createStep());
+    steps.add(new TextStep.TextInnerStepBuilder().setContext(context)
+        .setDataKey(DATA_KEY_EMAIL)
+        .setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+        .setTitleResId(R.string.email)
+        .setErrorResId(R.string.email_error)
+        .setDetailsResId(R.string.email_details)
+        .setChecker(new TextStep.StepChecker() {
+          @Override
+          public boolean check(String input) {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(input).matches();
+          }
+        })
+        .createStep());
+    steps.add(new TextStep.TextInnerStepBuilder().setContext(context)
+        .setDataKey(DATA_KEY_PASSWORD)
+        .setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)
+        .setTitleResId(R.string.password)
+        .setErrorResId(R.string.password_error)
+        .setDetailsResId(R.string.password_details)
+        .setChecker(new TextStep.StepChecker() {
+          @Override
+          public boolean check(String input) {
+            return input.length() >= 5;
+          }
+        })
+        .createStep());
+    steps.add(new DateStep.DateStepBuilder().setContext(context)
+        .setDataKey(DATA_KEY_BIRTHDAY)
+        .setTitleResId(R.string.birthday)
+        .setErrorResId(R.string.birthday_error)
+        .setDetailsResId(R.string.birthday_details)
+        .setDateChecker(new DateStep.DateStepChecker() {
+          @Override
+          public boolean check(int year, int month, int day) {
+            Calendar today = new GregorianCalendar();
+            Calendar birthday = new GregorianCalendar(year, month, day);
+            today.add(Calendar.YEAR, -14);
+            return today.after(birthday);
+          }
+        })
+        .createStep());
+    steps.add(new SeekBarStep.SeekBarStepBuilder().setContext(context)
+        .setDataKey(DATA_KEY_HEIGHT)
+        .setMax(180)
+        .setMin(150)
+        .setTitleResId(R.string.height)
+        .setErrorResId(R.string.height_error)
+        .setDetailsResId(R.string.height_details)
+        .setChecker(new SeekBarStep.StepChecker() {
+          @Override
+          public boolean check(int progress) {
+            return progress >= 160;
+          }
+        })
+        .createStep());
 
-	@Override
-	protected void onFormFinished(Bundle data){
-		Toast.makeText(this, "Form finished: " +
-				CheckBoxStep.checked(data, DATA_KEY_EULA) + ", " +
-                TextStep.text(data, DATA_KEY_EMAIL) + ", " +
-				TextStep.text(data, DATA_KEY_PASSWORD) + ", " +
-				DateStep.day(data, DATA_KEY_BIRTHDAY) + "." + DateStep.month(data, DATA_KEY_BIRTHDAY) + "." + DateStep.year(data, DATA_KEY_BIRTHDAY) + ", " +
-				SeekBarStep.progress(data, DATA_KEY_HEIGHT) + ", " +
-				TextStep.text(data, DATA_KEY_CITY),
-				Toast.LENGTH_LONG).show();
-		Log.d("MainActivity", "data: " + data.toString());
-	}
+    steps.add(new OptionStep.OptionStepBuilder(this).setDataKey(DATA_KEY_OPTIONS)
+        .setTitleResId(R.string.eula_title)
+        .setDetailsResId(R.string.eula_details)
+        .setErrorResId(R.string.eula_error)
+        .setOptions(new String[] { "Option 1", "Option 2", "Option 3" })
+        .createStep());
+
+
+    Log.d("MPB","TEST: "+(System.currentTimeMillis()-startTime));
+    return steps;
+  }
+
+  @Override
+  protected void onFormFinished(Bundle data) {
+    Toast.makeText(this, "Form finished: " +
+        CheckBoxStep.checked(data, DATA_KEY_EULA) + ", " +
+        TextStep.text(data, DATA_KEY_EMAIL) + ", " +
+        TextStep.text(data, DATA_KEY_PASSWORD) + ", " +
+        DateStep.day(data, DATA_KEY_BIRTHDAY) + "." + DateStep.month(data, DATA_KEY_BIRTHDAY) + "."
+        + DateStep.year(data, DATA_KEY_BIRTHDAY) + ", " +
+        SeekBarStep.progress(data, DATA_KEY_HEIGHT), Toast.LENGTH_LONG).show();
+    Log.d("MainActivity", "data: " + data.toString());
+  }
 }
